ce6c81dc26289c279f03e9f11642d78a9e898be9
Yaroslav Novitsky <rcr3211@gmail.com>
2022-04-16 02:02:59 +0300

    full fml events

--- src/main/java/com/github/mjaroslav/reponame/REPONAMEMod.java
+++ src/main/java/com/github/mjaroslav/reponame/REPONAMEMod.java
@@ -3,9 +3,7 @@ package com.github.mjaroslav.reponame;
 import com.github.mjaroslav.reponame.common.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.*;
 import org.jetbrains.annotations.NotNull;
 
 import static com.github.mjaroslav.reponame.lib.ModInfo.*;
@@ -20,6 +18,11 @@ public class REPONAMEMod {
     public static REPONAMEMod instance;
 
     @Mod.EventHandler
+    public void constr(@NotNull FMLConstructionEvent event) {
+        proxy.constr(event);
+    }
+
+    @Mod.EventHandler
     public void preInit(@NotNull FMLPreInitializationEvent event) {
         proxy.preInit(event);
     }
@@ -32,5 +35,40 @@ public class REPONAMEMod {
     @Mod.EventHandler
     public void postInit(@NotNull FMLPostInitializationEvent event) {
         proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    public void interModComms(@NotNull FMLInterModComms.IMCEvent event) {
+        proxy.interModComms(event);
+    }
+
+    @Mod.EventHandler
+    public void loadComplete(@NotNull FMLLoadCompleteEvent event) {
+        proxy.loadComplete(event);
+    }
+
+    @Mod.EventHandler
+    public void serverAboutToStart(@NotNull FMLServerAboutToStartEvent event) {
+        proxy.serverAboutToStart(event);
+    }
+
+    @Mod.EventHandler
+    public void serverStarting(@NotNull FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+
+    @Mod.EventHandler
+    public void serverStarted(@NotNull FMLServerStartedEvent event) {
+        proxy.serverStarted(event);
+    }
+
+    @Mod.EventHandler
+    public void serverStopping(@NotNull FMLServerStoppingEvent event) {
+        proxy.serverStopping(event);
+    }
+
+    @Mod.EventHandler
+    public void serverStopped(@NotNull FMLServerStoppedEvent event) {
+        proxy.serverStopped(event);
     }
 }
--- src/main/java/com/github/mjaroslav/reponame/common/CommonProxy.java
+++ src/main/java/com/github/mjaroslav/reponame/common/CommonProxy.java
@@ -1,11 +1,12 @@
 package com.github.mjaroslav.reponame.common;
 
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.*;
 import org.jetbrains.annotations.NotNull;
 
 public class CommonProxy {
+    public void constr(@NotNull FMLConstructionEvent event) {
+    }
+
     public void preInit(@NotNull FMLPreInitializationEvent event) {
     }
 
@@ -13,5 +14,26 @@ public class CommonProxy {
     }
 
     public void postInit(@NotNull FMLPostInitializationEvent event) {
+    }
+
+    public void interModComms(@NotNull FMLInterModComms.IMCEvent event) {
+    }
+
+    public void loadComplete(@NotNull FMLLoadCompleteEvent event) {
+    }
+
+    public void serverAboutToStart(@NotNull FMLServerAboutToStartEvent event) {
+    }
+
+    public void serverStarting(@NotNull FMLServerStartingEvent event) {
+    }
+
+    public void serverStarted(@NotNull FMLServerStartedEvent event) {
+    }
+
+    public void serverStopping(@NotNull FMLServerStoppingEvent event) {
+    }
+
+    public void serverStopped(@NotNull FMLServerStoppedEvent event) {
     }
 }
